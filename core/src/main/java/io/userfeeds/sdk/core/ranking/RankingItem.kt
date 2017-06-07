9124cd741b5fd37b4e0b44006cb1625471e86a24
Maciej Górski <maciek.gorski@gmail.com>
2017-06-07 11:19:05 +0200

    Change ranking item DTO to match what's returned from ads ranking.

--- core/src/main/java/io/userfeeds/sdk/core/ranking/RankingItem.kt
+++ core/src/main/java/io/userfeeds/sdk/core/ranking/RankingItem.kt
@@ -5,16 +5,18 @@ import android.os.Parcelable
 import io.userfeeds.sdk.core.common.parcelableCreator
 
 data class RankingItem(
+        val target: String,
         val score: Double,
-        val value: String)
+        val title: String)
     :
         Parcelable {
 
     override fun describeContents() = 0
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(target)
         dest.writeDouble(score)
-        dest.writeString(value)
+        dest.writeString(title)
     }
 
     companion object {
@@ -22,6 +24,7 @@ data class RankingItem(
         @JvmField
         val CREATOR = parcelableCreator {
             RankingItem(
+                    readString(),
                     readDouble(),
                     readString()
             )
