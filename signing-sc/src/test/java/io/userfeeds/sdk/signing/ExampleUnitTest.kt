14543fc186a56626dcbc0af7621bc41a93849a32
Maciej Górski <maciek.gorski@gmail.com>
2017-06-20 11:30:57 +0200

    Add empty signing module with SpongyCastle dependency.

---
+++ signing-sc/src/test/java/io/userfeeds/sdk/signing/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package io.userfeeds.sdk.signing
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
